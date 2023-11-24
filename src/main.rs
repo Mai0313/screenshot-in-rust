f9d1668aea90421938ffc4461756796bc377cfdc
Wei <mai@mai0313.com>
2023-11-24 08:08:29 +0800

    Update dependencies and refactor screenshot saving method

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,48 @@
 use scrap::{Capturer, Display};
-use std::io::ErrorKind::WouldBlock;
-use std::time::Duration;
-use std::thread::sleep;
-use image::jpeg::JPEGEncoder;
-use image::ColorType;
 use std::fs::File;
+use std::io::ErrorKind::WouldBlock;
 use std::path::Path;
+use std::thread;
+use std::time::{Duration, SystemTime};
+use md5;
+use image::{ImageBuffer, Rgba, ColorType};
+use image::codecs::png::PngEncoder;
+use image::ImageEncoder; // 新增導入
 
 fn main() {
-    // 獲取主螢幕
-    let display = Display::primary().expect("Couldn't find primary display.");
-    let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
-    let (w, h) = (capturer.width(), capturer.height());
-
     loop {
-        // 等待直到我們有一個螢幕快照
-        let buffer = loop {
+        let output_path_folder = "outputs";
+        let capture_percent = 1.0;
+
+        let display = Display::primary().expect("Couldn't find primary display.");
+        let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
+        
+        let (width, height) = (capturer.width(), capturer.height());
+        let capture_width = (width as f32 * capture_percent) as u32;
+        let capture_height = (height as f32 * capture_percent) as u32;
+
+        let frame = loop {
             match capturer.frame() {
-                Ok(buffer) => break buffer,
+                Ok(f) => break f,
                 Err(error) => {
-                    if error.kind() == WouldBlock {
-                        // 等待一會兒再試
-                        sleep(Duration::from_millis(100));
-                        continue;
-                    } else {
-                        // 其他錯誤
-                        panic!("Error: {}", error);
+                    if error.kind() != WouldBlock {
+                        panic!("Capture error: {}", error);
                     }
+                    thread::sleep(Duration::from_millis(1));
                 }
-            };
+            }
         };
 
-        // 獲取當前時間並格式化為檔案名稱
-        let filename = format!("outputs/{}.jpg", chrono::Local::now().format("%Y%m%d%H%M%S"));
+        let filename = format!("{:x}.png", md5::compute(format!("{:?}", SystemTime::now())));
+        let path = Path::new(output_path_folder).join(filename);
+        let mut file = File::create(&path).expect("Couldn't create file.");
 
-        // 將快照保存到檔案
-        let output_file = File::create(&Path::new(&filename)).unwrap();
-        let mut encoder = JPEGEncoder::new_with_quality(output_file, 80);
-        encoder.encode(&buffer, w as u32, h as u32, ColorType::RGBA(8)).unwrap();
+        let buffer = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(capture_width, capture_height, frame.to_vec()).expect("Couldn't create image buffer.");
+        
+        // 使用新的方法保存圖像
+        PngEncoder::new(&mut file).write_image(&buffer, capture_width, capture_height, ColorType::Rgba8).expect("Couldn't encode PNG.");
 
-        println!("Screenshot saved to {}", filename);
-
-        // 等待一秒
-        sleep(Duration::from_secs(1));
+        println!("Screenshot saved to {} at {:?}", output_path_folder, SystemTime::now());
+        thread::sleep(Duration::from_secs(1));
     }
 }
